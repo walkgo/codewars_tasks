e14c6818668b3ff4e692cc84219784e6a3b68be5
walkgo <bazanov.dev@gmail.com>
2022-01-13 22:02:03 +0400

    +1

---
+++ Convert boolean values to strings 'Yes' or 'No'.py
@@ -0,0 +1,10 @@
+def bool_to_word(boolean):
+    if boolean is True:
+        return("Yes")
+    else:
+        return("No")
+
+
+# Best Practices
+def bool_to_word(bool):
+    print("Yes" if bool else "No")
