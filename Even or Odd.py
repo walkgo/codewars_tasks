e1145010aca75b5d806fbb0115d53e67d753f6fe
walkgo <bazanov.dev@gmail.com>
2022-01-13 15:44:37 +0400

    +1

---
+++ Even or Odd.py
@@ -0,0 +1,10 @@
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+
+
+# Best Practices
+def even_or_odd(number):
+    return 'Odd' if number % 2 else 'Even'
