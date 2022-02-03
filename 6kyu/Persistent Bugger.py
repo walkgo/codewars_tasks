9c86d89aed98ff6e10f237b2f554bf48ec1965ba
walkgo <bazanov.dev@gmail.com>
2022-02-03 23:34:10 +0400

    +1

--- 6kyu/Persistent Bugger.py
+++ 6kyu/Persistent Bugger.py
@@ -11,10 +11,10 @@ def persistence(n):
 
 
 # Best Practices (I doubt :/)
-import operator
-def persistence(n):
-    i = 0
-    while n>=10:
-        n=reduce(operator.mul,[int(x) for x in str(n)],1)
-        i+=1
-    return i
+# import operator
+# def persistence(n):
+#     i = 0
+#     while n>=10:
+#         n=reduce(operator.mul,[int(x) for x in str(n)],1)
+#         i+=1
+#     return i
